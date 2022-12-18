90f7ea2a357b9ac22e236f7f8a3a6203e05c1464
Adhi Ravishankar <adhiravishankar@gmail.com>
2022-12-17 19:07:35 -0700

    Add Spring Project

---
+++ java/src/main/java/com/adhiravishankar/citiesorganizer/CitiesOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.adhiravishankar.citiesorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesOrganizerApplication.class, args);
+	}
+
+}
---
+++ java/src/test/java/com/adhiravishankar/citiesorganizer/CitiesOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adhiravishankar.citiesorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
