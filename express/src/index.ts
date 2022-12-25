ec8854dc4762f7e2d3137220b2498408c092c2d3
Adhi Ravishankar <adhiravishankar@gmail.com>
2022-12-24 19:07:01 -0700

    Put everything into src folder

--- express/src/index.ts
+++ express/src/index.ts
@@ -1,4 +1,3 @@
-import cookieParser from 'cookie-parser';
 import express from 'express';
 import logger from 'morgan';
 import path from 'path';
@@ -11,7 +10,6 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
