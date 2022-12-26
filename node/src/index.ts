3544885fc6fd39abdcf29bc9fd8ea289e852395d
Adhi Ravishankar <adhiravishankar@gmail.com>
2022-12-25 19:57:59 -0700

    Add Mongo Schemas and AWS SDK

--- node/src/index.ts
+++ node/src/index.ts
@@ -1,6 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+import dotenv from 'dotenv';
 
 import router from './routes/cities';
+
+dotenv.config();
+
+const mongooseClient = new mongoose.Mongoose();
+// eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
+// @ts-ignore
+mongooseClient.connect(process.env.MONGODB_URL).catch(err => console.log(err));
 
 const app = express();
 app.use(router);
