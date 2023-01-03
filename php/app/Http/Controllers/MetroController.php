142d586d4c6c3650aa460005d321680376e148d3
Adhi Ravishankar <adhiravishankar@gmail.com>
2023-01-02 18:15:40 -0700

    Initial commit for PHP Part 2

--- php/app/Http/Controllers/MetroController.php
+++ php/app/Http/Controllers/MetroController.php
@@ -2,7 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+
 class MetroController extends Controller
 {
+
+    /**
+     * Show a list of metros
+     *
+     * @return Collection
+     */
+    public function getMetros(): Collection
+    {
+        return DB::table('metros')->get();
+    }
+
+    /**
+     * Show a metro
+     *
+     * @param $id
+     * @return object
+     */
+    public function readMetro($id): object
+    {
+        return DB::table('metros')->find($id)->first();
+    }
+
+
+    /**
+     * Show a metro
+     *
+     * @param $metro
+     * @return object
+     */
+    public function createMetro($metro): object
+    {
+        return DB::table('metros')->find($id)->first();
+    }
 
 }
