2d0f6e15cd9f4b5053b12b43b699567e808909b8
Adhi Ravishankar <adhiravishankar@gmail.com>
2023-01-28 10:33:33 -0700

    Rename line endings

--- android/app/src/main/java/com/adhiravishankar/citiesorganizer/adapters/MetrosListAdapter.java
+++ android/app/src/main/java/com/adhiravishankar/citiesorganizer/adapters/MetrosListAdapter.java
@@ -1,11 +1,15 @@
 package com.adhiravishankar.citiesorganizer.adapters;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.ListAdapter;
 
+import com.adhiravishankar.citiesorganizer.activities.MetroDetails;
 import com.adhiravishankar.citiesorganizer.databinding.MetroCardBinding;
 import com.adhiravishankar.citiesorganizer.modeldiffs.MetroDiff;
 import com.adhiravishankar.citiesorganizer.models.Metro;
@@ -37,6 +41,17 @@ public class MetrosListAdapter extends ListAdapter<Metro, MetrosListRecyclerView
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
         viewHolder.getTextView().setText(this.getItem(position).getName());
+        String uuid = this.getItem(position).getID();
+        String metroName = this.getItem(position).getName();
+        Context context = viewHolder.itemView.getContext();
+        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, MetroDetails.class);
+                intent.putExtra("id", uuid);
+                intent.putExtra("name", metroName);
+                viewHolder.itemView.getContext().startActivity(intent);
+            }
+        });
     }
-
 }
